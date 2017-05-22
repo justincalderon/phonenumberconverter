0237f8d6fa297a00bfade695b70f7a90b6a55841
justincalderon <justin.calderon@outlook.com>
2017-05-21 22:09:05 -0700

    Complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function formatPhone() {
   var phoneInput = document.getElementById("phoneInput").value
-  var regExPhone = /^[0-9{3}]+[0-9{3}]+[0-9]{4}$/
+  var regExPhone = /\(?\d{3}[-.)]\d{3}[-.]\d{4}/;
   console.log(regExPhone.test(phoneInput));
 }
 
 document.getElementById("inputBtn").addEventListener("click", formatPhone)
+
+
+// [\w]+@\w+\.(net|com|edu)
